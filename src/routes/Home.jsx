c30493e8b6d95c16cb08b708fd2e4c7194e00ed5
Elson Otake <elsonotake@yahoo.com.br>
2026-09-23 16:50:13 -0300

    fix React import

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint linebreak-style: ["error", "windows"] */
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
